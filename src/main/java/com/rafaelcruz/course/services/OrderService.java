d62c93566cf9c4b43dd9ec36504c59018a12246d
Rafael Cruz <rafaeloribata@gmail.com>
2022-11-01 13:02:45 -0300

    Order, instant, ISO 8601

---
+++ src/main/java/com/rafaelcruz/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.rafaelcruz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelcruz.course.entities.Order;
+import com.rafaelcruz.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
